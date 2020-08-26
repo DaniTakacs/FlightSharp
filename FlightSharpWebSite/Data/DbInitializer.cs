da38a5b1173a46af1f7b96ebe03a86f2c6dfa3c3
Rhyvan <polyakan@gmail.com>
2020-08-26 22:16:01 +0200

    add EF6's way of data seeding

---
+++ FlightSharpWebSite/Data/DbInitializer.cs
@@ -0,0 +1,45 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlightSharpWebSite.Models;
+
+namespace FlightSharpWebSite.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(ApplicationContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.WebUsers.Any())
+            {
+                return;
+            }
+
+            var webUsers = new WebUser[]
+            {
+                new WebUser
+                {
+                    Email="testmintaanna@gmail.com",
+                    PasswordHashed="atmNotHashed1",
+                    Salt="salt1",
+                    UserAccount = new UserAccount{UserName="MintaAnna"},
+                },
+                new WebUser
+                {
+                    Email = "testadminpeter@gmail.com",
+                    PasswordHashed = "atmNotHashed2",
+                    Salt = "salt2",
+                    UserAccount = new UserAccount{UserName="AdminPeter", WebUserId=2},
+                },
+            };
+
+            foreach (var user in webUsers)
+            {
+                context.WebUsers.Add(user);
+            }
+            context.SaveChanges();
+        }
+    }
+}
