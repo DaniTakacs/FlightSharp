0e9e55619844fab90e0f8196aae9c19cf168ee58
Rhyvan <polyakan@gmail.com>
2020-08-26 22:12:50 +0200

    Set EF core SQL provider #27

---
+++ FlightSharpWebSite/Data/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+using FlightSharpWebSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlightSharpWebSite.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<WebUser> WebUsers { get; set; }
+        public DbSet<UserAccount> UserAccounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WebUser>()
+                .HasOne<UserAccount>(fk => fk.UserAccount)
+                .WithOne(ad => ad.WebUser)
+                .HasForeignKey<UserAccount>(ad => ad.WebUserId); 
+        }
+    }
+}
