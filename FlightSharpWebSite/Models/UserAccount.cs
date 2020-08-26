6995fcf9c4168f32bc7f9ed8512b1f2fe33f3a03
Rhyvan <polyakan@gmail.com>
2020-08-26 22:14:56 +0200

    add basic Model: userAccount #50

---
+++ FlightSharpWebSite/Models/UserAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightSharpWebSite.Models
+{
+    public class UserAccount
+    {
+        public int UserAccountId { get; set; }
+        public string UserName { get; set; }
+
+        public int WebUserId { get; set; }
+        public WebUser WebUser { get; set; }
+    }
+}
